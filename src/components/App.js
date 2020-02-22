16c1f22f0e2f9583f0a7e6cadf5bd23e139745af
nik <nik_mak@bk.ru>
2020-02-22 23:00:32 +0300

    vote page fix stepper

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,12 @@ import React, {Component} from 'react';
 import '../assets/css/App/App.css';
 import StartScreen from "./screens/StartScreen";
 import Footer from "./combined/Footer";
-import Form from "./combined/Form";
 import ruCompanies from "./../assets/json/ru_companies";
 import enCompanies from "./../assets/json/en_companies";
 import questions from "./../assets/json/questions";
 import VoteScreen from "./screens/VoteScreen";
 
-export let ALL_STEPS = questions.lenght;
+export let ALL_STEPS = questions.length * 2;
 export const RU_NOMINATION = "Российская авиакомпания года - лидер пассажирских симпатий";
 export const EN_NOMINATION = "Зарубежная авиакомпания года - лидер пассажирских симпатий";
 export const NOMINATIONS = {
@@ -47,7 +46,7 @@ class App extends Component {
                 "handler":this.screenHandler,
                 "companies":COMPANIES[lang],
                 "question":questions[i],
-                "step": questions.length * part + i
+                "step": questions.length * part + i + 1
             };
             if(i !== 0){
                 props.selected = this.state.data[makeName(NOMINATIONS[lang], questions[0])]
--- src/components/combined/Stars.js
+++ src/components/combined/Stars.js
@@ -1,4 +1,4 @@
-import React, {Component, Fragment} from "react";
+import React, {Component} from "react";
 import Star from "../base/Star";
 
 class Stars extends Component {
