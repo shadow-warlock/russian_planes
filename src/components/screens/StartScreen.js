8091fa90afb922ed24856dbc6489e774858a88f8
timager <timger98@gmail.com>
2020-02-22 17:40:41 +0300

    start screen

--- src/components/screens/StartScreen.js
+++ src/components/screens/StartScreen.js
@@ -1,8 +1,26 @@
 import React, {Component} from "react";
+import logoImage from "./../../assets/img/logo.svg"
+import "./../../assets/css/start_screen.css"
 
 class StartScreen extends Component {
     render() {
-        return ("");
+        return (
+            <div className={"start_screen default_padding"}>
+                <div>
+                    <img src={logoImage} alt={"logo"}/>
+                </div>
+                <div className={"start_screen_content"}>
+                    <div>
+                        <div>в рамках 23-й <br/> национальной авиационной премии «Крылья России</div>
+                        <div className={"blue header"}>Народное голосование. Выбираем лучшую авиакомпанию 2019</div>
+                        <div>
+                            <div>ЗВЕЗДЫ</div>
+                            <button>Проголосуй и выиграй приз*</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
